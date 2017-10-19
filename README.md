e992d643f30309bb686e3ddc6f0d18b9cab76ffd
Joe Luensmann <jtluensmann@gmail.com>
2017-10-18 22:16:16 -0400

    Version 1.0, basic functions complete

--- README.md
+++ README.md
@@ -3,4 +3,12 @@ color on mouseover.
 
 I created the layout using the CSS grid to dynamically change the size of the "board" based on the current value of the CSS variable "rowNum."  Initially it's set to 16 squares per side, but it can be increased and the CSS grid will automatically calculate the size of the new grid via the "grid-template-rows" and "grid-template-columns" attributes.  All these things have already been implemented.
 
-The next steps are to take input from the user via UI buttons to reset the grid and to resize it.  I'm not sure how extreme the values can get, so testing is required to see where it breaks and keep the input within valid ranges.
+Next I implemented buttons to resize the grid dynamically and to clear all current entries.  I also fixed a lingering bug where, when resized, the grid cells would get smaller and smaller inside of the container div -- I solved it by removing all previous cells before drawing new ones in the initializeGrid() method.
+
+V1.0
+
+If I wanted to work on this some more, I could implement the feature where each cell turns a different color, and on each pass with the mouse the color gets 10% darker until it's black.  I'm confident that I *could* do this to the point where I feel like I don't *need* to.
+
+The other features I could work on are styling the elements more thoroughly and making the entire grid responsive, but for now, I feel like I've done a good job on this assignment.
+
+--jtl 10/17/17
--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,17 +1,52 @@
-window.onload = function() {
-  let htmlStyles = window.getComputedStyle(document.querySelector("html"));
-  let rowNum = parseInt(htmlStyles.getPropertyValue("--rowNum"));
-
-  for(var i = 0; i < rowNum; i++) {
-    for(var j = 0; j < rowNum; j++) {
+function initializeGrid(length) {
+  for(let i = 0; i < length; i++) {
+    for(let j = 0; j < length; j++) {
       $("<div>", {class: "cell"}).appendTo("#wrapper");
     }
   }
+  let cw = $(".cell").width();
+  $(".cell").css({'height':cw+'px'})
+}
 
-  $(".cell").hover(function() {
-    $(this).addClass("pressed");
+function clearGrid() {
+  $(".cell").each(function() {
+    $(this).removeClass("pressed");
   });
 }
+
+$(document).ready(function() {
+  //retrieve the "gridLength" variable from CSS
+  let htmlStyles = window.getComputedStyle(document.querySelector("html"));
+  let gridLength = parseInt(htmlStyles.getPropertyValue("--gridLength"));
+
+  initializeGrid(gridLength);
+
+
+  $("div").on("mouseover", ".cell", (function() {
+    $(this).addClass("pressed");
+  }));
+
+  $("#clear").click(function() {
+    clearGrid();
+  });
+
+  $("#resize").click(function() {
+    let length = prompt("Enter a new length for your Etch-A-Sketch.")
+    while(length <= 0 || length >= 50) {
+      length = prompt("Please enter a value between 1 and 50")
+    }
+    document.documentElement.style.setProperty("--gridLength", length);
+
+    $(".cell").remove();
+    gridLength = length;
+    initializeGrid(gridLength);
+    clearGrid();
+  });
+
+
+
+});
+
 
 
 /**
